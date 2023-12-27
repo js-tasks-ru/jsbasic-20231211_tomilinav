445b20f0efb1ed24869a5f333cf8fbce62f610e2
Tomilin Artem <TomilinAV@progressagro.com>
2023-12-27 14:53:56 +0300

    Module 1 task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
   // ваш код...
+
+  let lowerStr = str.toLowerCase();
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx') ;
+
+
 }
+alert (checkSpam('1XbeT now')===true)
+alert (checkSpam(' xxx')===true)
+alert (checkSpam('innocent rabbit')===false)
+
+
